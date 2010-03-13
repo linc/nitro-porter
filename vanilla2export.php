b79a5bc2157e59cb3c7f59b73918f20bbe97ca6e
lincolnwebs <lincoln@icrontic.com>
2010-03-13 17:42:22 -0500

    more logic

---
+++ vanilla2export.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * All-purpose logic
+ *
+ * @copyright Vanilla Forums Inc. 2010
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU GPL2
+ * @package VanillaPorter
+ */
+
+/** @var array Supported forum packages: classname => DisplayName */
+$supported = array(
+   'vbulletin' => array('name'=>'vBulletin 3+', 'prefix'=>'vb_');
+
+/** Test filesystem permissions */  
+function TestWrite() {
+   // Create file
+   $file = 'vanilla2test.txt';
+   @touch($file);
+   if(is_writable($file)) {
+      @unlink($file);
+      return true;
+   }
+   else return false;
+}
+
+// Files
+include('class.exportmodel.php');
+include('class.exportviews.php');
+include('class.exportcontroller.php');
+foreach($supported as $file => $info) {
+   include('class.'.$file.'.php');
+}
+
+// Logic
+if(isset($_POST['type']) && array_key_exists($_POST['type'], $supported)) {
+   // Mini-Factory
+   $class = ucwords($_POST['type']);
+   new $class;
+}
+else {
+   // View form or error
+   $View = new ExportViews;
+   if(TestWrite())
+      $View->InfoForm($supported);
+   else
+      $View->NoPermission();
+}
