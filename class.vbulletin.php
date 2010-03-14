26b1deb5cd098b0c483d7b2aceeccde7741cab18
lincolnwebs <lincoln@icrontic.com>
2010-03-14 12:51:23 -0400

    user mapping

--- class.vbulletin.php
+++ class.vbulletin.php
@@ -18,13 +18,38 @@ class Vbulletin extends ExportController {
     * Forum-specific export format
     */
    public function ForumExport() {
-      $Ex->BeginExport(PATH_ROOT.DS.'uploads'.DS.'export '.date('Y-m-d His').'.txt.gz', 'Vanilla 2.0');
-      $Ex->ExportTable('User', 'select * from :_User'); // ":_" will be replace by database prefix
-      $Ex->ExportTable('Role', 'select * from :_Role');
-      $Ex->ExportTable('UserRole', 'select * from :_UserRole');
-      $Ex->ExportTable('Category', 'select * from :_Category');
-      $Ex->ExportTable('Discussion', 'select * from :_Discussion');
-      $Ex->ExportTable('Comment', 'select * from :_Comment');
+      // Begin
+      $Ex->BeginExport(PATH_ROOT.DS.'uploads'.DS.'export '.date('Y-m-d His').'.txt.gz', 'vBulletin 3+');
+      
+      // Users
+      $User_Map = array(
+         'UserID'=>'userid',
+         'Name'=>'username',
+         'Password'=>'password',
+         'Email'=>'email',
+         'ShowEmail'=>'userid',
+         'InviteUserID'=>'referrerid',
+         'DateOfBirth'=>'userid',
+         'HourOffset'=>'timezoneoffset',
+         'CountComments'=>'posts');
+      $Ex->ExportTable('User', "select *,
+            DATE_FORMAT(birthday_search,GET_FORMAT(DATE,'ISO')) as DateOfBirth,
+            FROM_UNIXTIME(joindate) as DateFirstVisit,
+            FROM_UNIXTIME(lastvisit) as DateLastActive,
+            FROM_UNIXTIME(joindate) as DateInserted,
+            FROM_UNIXTIME(lastactivity) as DateUpdated,
+            (SELECT COUNT(*) FROM ".$Ex->Prefix."thread WHERE postuserid=userid) as CountDiscussions,
+            salt as VbulletinSalt
+         from :_User", $User_Map);  // ":_" will be replace by database prefix
+      
+      
+      //$Ex->ExportTable('Role', 'select * from :_Role');
+      //$Ex->ExportTable('UserRole', 'select * from :_UserRole');
+      //$Ex->ExportTable('Category', 'select * from :_Category');
+      //$Ex->ExportTable('Discussion', 'select * from :_Discussion');
+      //$Ex->ExportTable('Comment', 'select * from :_Comment');
+      
+      // End
       $Ex->EndExport();
    }
    
