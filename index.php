baba0ccc4a769e036e6811434a70784544d4ea81
Todd Burry <todd@Todd-Burrys-MacBook-Pro.local>
2010-05-18 00:20:20 -0400

    Added full GPL license to top of index.php to comply with the rules of the GPL. Added an "automatically generated" line to the output of make.php. Switched html comments to php comments in make.php

--- index.php
+++ index.php
@@ -5,8 +5,13 @@
  * To use this script copy it to your web server and open it in your browser.
  * If you have a larger database the directory should be writable so that the export file can be saved locally and zipped.
  *
- * @copyright Vanilla Forums Inc. 2010
- * @license http://opensource.org/licenses/gpl-2.0.php GNU GPL2
+ * Copyright 2010 Vanilla Forums Inc.
+ * This file is part of Garden.
+ * Garden is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ * Garden is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with Garden.  If not, see <http://www.gnu.org/licenses/>.
+ * Contact Vanilla Forums Inc. at support [at] vanillaforums [dot] com
+ *
  * @package VanillaPorter
  */
  
