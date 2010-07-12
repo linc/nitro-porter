da668945335fe38b4d904a396d48ee8df6e01b77
Todd Burry <todd@vanillaforums.com>
2010-07-12 11:47:12 -0400

    Added border-radius properties to the css. Added utf8 encoding to the <?xml...?>.

--- views.php
+++ views.php
@@ -12,7 +12,9 @@
  * HTML header
  */
 function PageHeader() {
-   ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+   echo '<?xml version="1.0" encoding="UTF-8"?>';
+      ?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
 <head>
    <title>Vanilla Porter - Forum Export Tool</title>
