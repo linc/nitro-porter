984666d14ca0eb13e40bf14ab11b268cebd5893c
lincolnwebs <lincolnwebs@gmail.com>
2014-11-20 10:06:44 -0500

    My IDE won't shut up about this line

--- packages/class.yaf.php
+++ packages/class.yaf.php
@@ -224,7 +224,7 @@ class Yaf extends ExportController {
          $Method = self::$PasswordFormats[$Method];
       else
          $Method = 'sha1';
-      $Result = "$Method$$Salt$$Hash$";
+      $Result = $Method.'$'.$Salt.'$'.$Hash.'$';
       return $Result;
    }
    
