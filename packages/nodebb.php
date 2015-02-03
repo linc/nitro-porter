2673445fb28576706c07ab59d894c597310a76a5
initvector <ryan.p@vanillaforums.com>
2015-02-03 18:38:31 -0500

    NodeBB: Removing hardcoded GDN table prefixes

--- packages/nodebb.php
+++ packages/nodebb.php
@@ -53,7 +53,7 @@ class Nodebb extends ExportController {
         $Ex->ExportTable('User', "
 
              select uid, username, password, email, `email:confirmed` as confirmed, showemail, joindate, lastonline, lastposttime, banned, 0 as admin, 'crypt' as hm
-             from gdn_user
+             from :_user
 
              ", $User_Map);
 
@@ -67,7 +67,7 @@ class Nodebb extends ExportController {
         $Ex->ExportTable('Role', "
 
             select gm._key as _key, gm._num as _num, g.description as description
-            from gdn_group_members gm left join gdn_group g
+            from :_group_members gm left join :_group g
             on gm._key like concat(g._key, '%')
 
             ", $Role_Map);
@@ -80,7 +80,7 @@ class Nodebb extends ExportController {
         $Ex->ExportTable('UserRole', "
 
             select *, g._num as id
-            from gdn_group_members g join gdn_group_members__members m
+            from :_group_members g join :_group_members__members m
             on g._id = m._parentid
 
         ", $UserRole_Map);
@@ -95,25 +95,25 @@ class Nodebb extends ExportController {
         $Ex->ExportTable('UserMeta', "
 
             select uid, 'Plugin.Signatures.Sig' as name, signature
-            from gdn_user
+            from :_user
             where length(signature) > 1
 
             union
 
             select uid, 'Plugin.Signatures.Format', 'Markdown'
-            from gdn_user
+            from :_user
             where length(signature) > 1
 
             union
 
             select uid, 'Profile.Website' as name, website
-            from gdn_user
+            from :_user
             where length(website) > 7
 
             union
 
             select uid, 'Profile.Location' as name, location
-            from gdn_user
+            from :_user
             where length(location) > 1
 
         ", $UserMeta_Map);
@@ -133,18 +133,18 @@ class Nodebb extends ExportController {
         $Ex->ExportTable('Category', "
 
             select *
-            from gdn_category
+            from :_category
 
         ", $Category_Map);
 
-        if (!$Ex->IndexExists('z_idx_gdn_topic', 'gdn_topic')) {
-            $Ex->Query("create index z_idx_gdn_topic on gdn_topic(mainPid);");
+        if (!$Ex->IndexExists('z_idx_topic', ':_topic')) {
+            $Ex->Query("create index z_idx_topic on :_topic(mainPid);");
         }
-        if (!$Ex->IndexExists('z_idx_gdn_post', 'gdn_post')) {
-            $Ex->Query("create index z_idx_gdn_post on gdn_post(pid);");
+        if (!$Ex->IndexExists('z_idx_post', ':_post')) {
+            $Ex->Query("create index z_idx_post on :_post(pid);");
         }
-        if (!$Ex->IndexExists('z_idx_gdn_poll', 'gdn_poll')) {
-            $Ex->Query("create index z_idx_gdn_poll on gdn_poll(tid);");
+        if (!$Ex->IndexExists('z_idx_poll', ':_poll')) {
+            $Ex->Query("create index z_idx_poll on :_poll(tid);");
         }
 
         $Ex->Query("drop table if exists z_discussionids;");
@@ -163,7 +163,7 @@ class Nodebb extends ExportController {
                 tid
             )
             select mainPid
-            from gdn_topic
+            from :_topic
             where mainPid is not null
             and deleted != 1;
 
@@ -207,7 +207,7 @@ class Nodebb extends ExportController {
 
             insert z_reactiontotalsupvote
             select value, count(*) as totals
-            from gdn_uid_upvote
+            from :_uid_upvote
             group by value;
 
         ");
@@ -216,7 +216,7 @@ class Nodebb extends ExportController {
 
             insert z_reactiontotalsdownvote
             select value, count(*) as totals
-            from gdn_uid_downvote
+            from :_uid_downvote
             group by value;
 
         ");
@@ -263,14 +263,14 @@ class Nodebb extends ExportController {
         $Ex->ExportTable('Discussion', "
 
             select p.tid, cid, title, content, p.uid, locked, pinned, p.timestamp, p.edited, p.editor, viewcount, votes, poll._id as poll, 'Markdown' as format, concat(ifnull(u.total, 0), ':', ifnull(d.total, 0)) as attributes
-            from gdn_topic t
-            left join gdn_post p
+            from :_topic t
+            left join :_post p
             on t.mainPid = p.pid
             left join z_reactiontotalsupvote u
             on u.value = t.mainPid
             left join z_reactiontotalsdownvote d
             on d.value = t.mainPid
-            left join gdn_poll poll
+            left join :_poll poll
             on p.tid = poll.tid
             where t.deleted != 1
 
@@ -308,7 +308,7 @@ class Nodebb extends ExportController {
                 votes
             )
             select p.pid, p.content, p.uid, p.tid, p.timestamp, p.edited, p.editor, p.votes
-            from gdn_post p
+            from :_post p
             left join z_discussionids t
             on t.tid = p.pid
             where p.deleted != 1 and t.tid is null;
@@ -357,7 +357,7 @@ class Nodebb extends ExportController {
         $Ex->ExportTable('Poll', "
 
             select *
-            from gdn_poll p left join gdn_poll_settings ps
+            from :_poll p left join :_poll_settings ps
             on ps._key like concat(p._key, ':', '%')
 
         ", $Poll_Map);
@@ -374,7 +374,7 @@ class Nodebb extends ExportController {
         $Ex->ExportTable('PollOption', "
 
             select _num, _key, title, id+1 as sort, votecount, 'Html' as format
-            from gdn_poll_options
+            from :_poll_options
             where title is not null
 
         ", $PollOption_Map);
@@ -387,18 +387,18 @@ class Nodebb extends ExportController {
         $Ex->ExportTable('PollVote', "
 
             select povm.members as userid, po._num as poll_option_id
-            from gdn_poll_options_votes__members povm
-            left join gdn_poll_options_votes pov
+            from :_poll_options_votes__members povm
+            left join :_poll_options_votes pov
             on povm._parentid = pov._id
-            left join gdn_poll_options po
+            left join :_poll_options po
             on pov._key like concat(po._key, ':', '%')
             where po.title is not null
 
         ", $PollVote_Map);
 
         //Tags
-        if (!$Ex->IndexExists('z_idx_gdn_topic_key', 'gdn_topic')) {
-            $Ex->Query("create index z_idx_gdn_topic_key on gdn_topic (_key);");
+        if (!$Ex->IndexExists('z_idx_topic_key', ':_topic')) {
+            $Ex->Query("create index z_idx_topic_key on :_topic (_key);");
         }
 
         $Tag_Map = array(
@@ -421,12 +421,12 @@ class Nodebb extends ExportController {
             select @rownr:=@rownr+1 as tagid, members as fullname, members as slug, '' as type, count, timestamp, uid, cid
             from (
                 select members, count(*) as count, _parentid
-                from gdn_topic_tags__members
+                from :_topic_tags__members
                 group by members
             ) as tags
-            join gdn_topic_tags tt
+            join :_topic_tags tt
             on tt._id = _parentid
-            left join gdn_topic t
+            left join :_topic t
             on substring(tt._key, 1, length(tt._key) - 5) = t._key
 
         ", $Tag_Map);
@@ -443,26 +443,26 @@ class Nodebb extends ExportController {
         $Ex->ExportTable('TagDiscussion', "
 
             select tagid, cid, tid, timestamp
-            from gdn_topic_tags__members two
+            from :_topic_tags__members two
             join (
                 select @rownr:=@rownr+1 as tagid, members as fullname, members as slug, count
                 from (
                     select members, count(*) as count
-                    from gdn_topic_tags__members
+                    from :_topic_tags__members
                     group by members
                 ) as tags
             ) as tagids
             on two.members = tagids.fullname
-            join gdn_topic_tags tt
+            join :_topic_tags tt
             on tt._id = _parentid
-            left join gdn_topic t
+            left join :_topic t
             on substring(tt._key, 1, length(tt._key) - 5) = t._key
 
         ", $TagDiscussion_Map);
 
         //Conversations
-        if (!$Ex->IndexExists('z_message_key', ':_message')) {
-            $Ex->Query("create index z_message_key on :_message(_key);");
+        if (!$Ex->IndexExists('z_idx_message_key', ':_message')) {
+            $Ex->Query("create index z_idx_message_key on :_message(_key);");
         }
         $Ex->Query("drop table if exists z_pmto;");
         $Ex->Query("
@@ -483,7 +483,7 @@ class Nodebb extends ExportController {
                 userid
             )
             select substring_index(_key, ':', -1), fromuid
-            from gdn_message;
+            from :_message;
 
         ");
 
@@ -494,7 +494,7 @@ class Nodebb extends ExportController {
                 userid
             )
             select substring_index(_key, ':', -1), touid
-            from gdn_message;
+            from :_message;
 
         ");
 
@@ -578,7 +578,7 @@ class Nodebb extends ExportController {
 
             select *, firstmessageid as conversationid, 2 as countparticipants
             from z_pmgroup
-            left join gdn_message
+            left join :_message
             on groupid = _key;
 
         ", $Conversation_Map);
@@ -597,7 +597,7 @@ class Nodebb extends ExportController {
 
             select groupid as conversationid, pmid as messageid, content, 'Text' as format, fromuid, timestamp
             from z_pmto2
-            left join gdn_message
+            left join :_message
             on concat('message:', pmid) = _key
 
         ", $ConversationMessage_Map);
@@ -626,20 +626,20 @@ class Nodebb extends ExportController {
 
         $Ex->ExportTable('UserDiscussion', "
             select members, _key, 1 as bookmarked
-            from gdn_tid_followers__members
-            left join gdn_tid_followers
+            from :_tid_followers__members
+            left join :_tid_followers
             on _parentid = _id
         ", $UserDiscussion_Map);
 
         //Reactions
-        if (!$Ex->IndexExists('z_idx_gdn_topic_mainpid', 'gdn_topic')) {
-            $Ex->Query("create index z_idx_gdn_topic_mainpid on gdn_topic(mainPid);");
+        if (!$Ex->IndexExists('z_idx_topic_mainpid', ':_topic')) {
+            $Ex->Query("create index z_idx_topic_mainpid on :_topic(mainPid);");
         }
-        if (!$Ex->IndexExists('z_idx_gdn_uid_downvote', 'gdn_uid_downvote')) {
-            $Ex->Query("create index z_idx_gdn_uid_downvote on gdn_uid_downvote(value);");
+        if (!$Ex->IndexExists('z_idx_uid_downvote', ':_uid_downvote')) {
+            $Ex->Query("create index z_idx_uid_downvote on :_uid_downvote(value);");
         }
-        if (!$Ex->IndexExists('z_idx_gdn_uid_upvote', 'gdn_uid_upvote')) {
-            $Ex->Query("create index z_idx_gdn_uid_upvote on gdn_uid_upvote(value);");
+        if (!$Ex->IndexExists('z_idx_uid_upvote', ':_uid_upvote')) {
+            $Ex->Query("create index z_idx_uid_upvote on :_uid_upvote(value);");
         }
 
         $UserTag_Map = array(
@@ -654,7 +654,7 @@ class Nodebb extends ExportController {
         $Ex->ExportTable('UserTag', "
 
             select 11 as tagid, 'Discussion' as recordtype, u._key, u.value, score, total
-            from gdn_uid_upvote u
+            from :_uid_upvote u
             left join z_discussionids t
             on u.value = t.tid
             left join z_reactiontotalsupvote r
@@ -665,7 +665,7 @@ class Nodebb extends ExportController {
             union
 
             select 11 as tagid, 'Comment' as recordtype, u._key, u.value, score, total
-            from gdn_uid_upvote u
+            from :_uid_upvote u
             left join z_discussionids t
             on u.value = t.tid
             left join z_reactiontotalsupvote r
@@ -676,7 +676,7 @@ class Nodebb extends ExportController {
             union
 
             select 10 as tagid, 'Discussion' as recordtype, u._key, u.value, score, total
-            from gdn_uid_downvote u
+            from :_uid_downvote u
             left join z_discussionids t
             on u.value = t.tid
             left join z_reactiontotalsdownvote r
@@ -687,7 +687,7 @@ class Nodebb extends ExportController {
             union
 
             select 10 as tagid, 'Comment' as recordtype, u._key, u.value, score, total
-            from gdn_uid_downvote u
+            from :_uid_downvote u
             left join z_discussionids t
             on u.value = t.tid
             left join z_reactiontotalsdownvote r
