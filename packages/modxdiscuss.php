7a3224e59c453e0053fa4fb5cf189d4e17c66ead
R-J <robin.jurinka@gmx.net>
2017-12-24 12:49:11 +0100

    Replace mappings with SQL where possible

--- packages/modxdiscuss.php
+++ packages/modxdiscuss.php
@@ -62,40 +62,26 @@ class MODXDiscuss extends ExportController {
         // Reiterate the platform name here to be included in the porter file header.
         $ex->beginExport('', 'MODX Discuss Extension', array('HashMethod' => 'Vanilla'));
 
-        // It's usually a good idea to do the porting in the approximate order laid out here.
-
         // User.
-        // Map as much as possible using the $x_Map array for clarity.
-        // Key is always the source column name.
-        // Value is either the destination column or an array of meta data, usually Column & Filter.
-        // If it's a meta array, 'Column' is the destination column name and 'Filter' is a method name to run it thru.
-        // Here, 'HTMLDecoder' is a method in ExportModel. Check there for available filters.
-        // Assume no filter is needed and only use one if you encounter issues.
-        $user_Map = array(
-            'user' => 'UserID',
-            'username' => 'Name',
-            'password' => 'Password',
-            'email' => 'Email',
-            'ip' => 'LastIPAddress',
-            'createdon' => 'DateInserted',
-            'gender2' => 'Gender', // 0 => 'u'
-            'birthdate' => 'DateOfBirth',
-            'location' => 'Location',
-            'confirmed' => 'Confirmed',
-            'last_active' => 'DateLastActive',
-            'title' => 'Title',
-            'avatar' => 'Photo',
-            'show_email' => 'ShowEmail',
-        );
-        // This is the query that the x_Map array above will be mapped against.
-        // Therefore, our select statement must cover all the "source" columns.
-        // It's frequently necessary to add joins, where clauses, and more to get the data we want.
-        // The :_ before the table name is the placeholder for the prefix designated. It gets swapped on the fly.
         $ex->exportTable('User', "
-         select u.*,
-         case u.gender when 0 then 'u' else gender end as gender2
-         from :_users u
-         ", $user_Map);
+            select
+                u.user as UserID,
+                u.username as Name,
+                u.password as Password,
+                u.email as Email,
+                u.ip as LastIPAddress,
+                u.createdon as DateInserted,
+                u.gender2 as Gender, // 0 => 'u'
+                u.birthdate as DateOfBirth,
+                u.location as Location,
+                u.confirmed as Confirmed,
+                u.last_active as DateLastActive,
+                u.title as Title,
+                u.avatar as Photo,
+                u.show_email as ShowEmail,
+                case u.gender when 0 then 'u' else gender end as gender2
+            from :_users u"
+        );
         
         /**
          *  No Roles in discuss, wiping the role table doesn't make sense!
@@ -117,13 +103,13 @@ class MODXDiscuss extends ExportController {
         // Really simple matchup.
         // Note that setting Admin=1 on the User table trumps all roles & permissions with "owner" privileges.
         // Whatever account you select during the import will get the Admin=1 flag to prevent permissions issues.
-        $userRole_Map = array(
-            'user' => 'UserID',
-            'roleID' => 'RoleID',
-        );
         $ex->exportTable('UserRole', "
-         select u.*, '32' as roleID 
-         from :_moderators u", $userRole_Map);
+            select
+                u.user as UserID,
+                u.roleID as RoleID,
+                '32' as roleID
+            from :_moderators u"
+        );
 
         // Permission.
         // Feel free to add a permission export if this is a major platform or it will see reuse.
@@ -135,39 +121,39 @@ class MODXDiscuss extends ExportController {
         // The Profile Extender addon uses the namespace "Profile.[FieldName]"
         // You can add the appropriately-named fields after the migration and profiles will auto-populate with the migrated data.
         $ex->exportTable('UserMeta', "
-         select
-            user as UserID,
-            'Plugin.Signatures.Sig' as `Name`,
-            Signature as `Value`
-         from :_users
-         where Signature <> ''
+            select
+                user as UserID,
+                'Plugin.Signatures.Sig' as `Name`,
+                Signature as `Value`
+            from :_users
+            where Signature <> ''
 
-         union
+            union
 
-         select
-            user as UserID,
-            'Profile.Website' as `Name`,
-            website as `Value`
-         from :_users
-         where website <> ''
+            select
+                user as UserID,
+                'Profile.Website' as `Name`,
+                website as `Value`
+            from :_users
+            where website <> ''
 
-         union
+            union
 
-         select
-            user as UserID,
-            'Profile.LastName' as `Name`,
-            name_last as `Value`
-         from :_users
-         where name_last <> ''
+            select
+                user as UserID,
+                'Profile.LastName' as `Name`,
+                name_last as `Value`
+            from :_users
+            where name_last <> ''
 
-         union
+            union
 
-         select
-            user as UserID,
-            'Profile.FirstName' as `Name`,
-            name_first as `Value`
-         from :_users
-         where name_first <> ''
+            select
+                user as UserID,
+                'Profile.FirstName' as `Name`,
+                name_first as `Value`
+            from :_users
+            where name_first <> ''
         ");
 
         // Category.
@@ -176,26 +162,26 @@ class MODXDiscuss extends ExportController {
         // Don't worry about rebuilding the TreeLeft & TreeRight properties. Vanilla can fix this afterward
         // if you just get the Sort and ParentIDs correct.
         $ex->exportTable('Category', "
-         select
-            id as CategoryID,
-            case parent when 0 then '-1' else parent end as ParentCategoryID,
-            name as `Name`,
-            description as `Description`,
-            'Heading' as `DisplayAs`,
+            select
+                id as CategoryID,
+                case parent when 0 then '-1' else parent end as ParentCategoryID,
+                name as `Name`,
+                description as `Description`,
+                'Heading' as `DisplayAs`,
             rank as `Sort`
-         from :_boards
+            from :_boards
 
-         union
+            union
 
-         select
-            (select max(id) from :_boards) + id as CategoryID,
-            '-1' as ParentCategoryID,
-            name as `Name`,
-            description as `Description`,
-            'Heading' as `DisplayAs`,
-            rank as `Sort`
-         from :_categories
-         ");
+            select
+                (select max(id) from :_boards) + id as CategoryID,
+                '-1' as ParentCategoryID,
+                name as `Name`,
+                description as `Description`,
+                'Heading' as `DisplayAs`,
+                rank as `Sort`
+            from :_categories
+        ");
 
         // Discussion.
         // A frequent issue is for the OPs content to be on the comment/post table, so you may need to join it.
@@ -204,46 +190,43 @@ class MODXDiscuss extends ExportController {
         );
         // It's easier to convert between Unix time and MySQL datestamps during the db query.
         $ex->exportTable('Discussion', "
-         select
-          t.id as `DiscussionID`,
-          t.board as `CategoryID`,
-          t.title as `title2`,
-          t.replies as `CountComments`,
-          t.views as `CountViews`,
-          t.locked as `Closed`,
-          t.sticky as `Announce`,
-          p.message as `Body`,
-          'BBCode' as `Format`,
-          p.author as `InsertUserID`,
-          p.createdon as `DateInserted`,
-          p.editedon as `DateUpdated`,
-          p.ip as `InsertIPAddress`,
-          case p.editedby when 0 then null else p.editedby end as `UpdateUserID`
-         from :_threads t
-         join :_posts p
-            on t.id = p.thread
-        ", $discussion_Map);
+            select
+                t.id as `DiscussionID`,
+                t.board as `CategoryID`,
+                t.title as `title2`,
+                t.replies as `CountComments`,
+                t.views as `CountViews`,
+                t.locked as `Closed`,
+                t.sticky as `Announce`,
+                p.message as `Body`,
+                'BBCode' as `Format`,
+                p.author as `InsertUserID`,
+                p.createdon as `DateInserted`,
+                p.editedon as `DateUpdated`,
+                p.ip as `InsertIPAddress`,
+                case p.editedby when 0 then null else p.editedby end as `UpdateUserID`
+            from :_threads t
+            join :_posts p on t.id = p.thread",
+        $discussion_Map);
 
         // Comment.
         // This is where big migrations are going to get bogged down.
         // Be sure you have indexes created for any columns you are joining on.
-        $comment_Map = array(
-            'id' => 'CommentID',
-            'thread' => 'DiscussionID',
-            'message' => 'Body',
-            'BBCode' => 'Format',
-            'author' => 'InsertUserID',
-            'createdon' => 'DateInserted',
-            'editedby2' => 'UpdateUserID',
-            'p.editedon' => 'DateUpdated',
-            'p.ip' => 'InsertIPAddress',
+        $ex->exportTable('Comment', '
+            select
+                p.id as CommentID,
+                p.thread as DiscussionID,
+                p.message as Body,
+                p.BBCode as Format,
+                p.author as InsertUserID,
+                p.createdon as DateInserted,
+                p.editedby2 as UpdateUserID,
+                p.editedon as DateUpdated,
+                p.ip as InsertIPAddress,
+                case p.editedby when 0 then null else p.editedby end as editedby2
+            from :_posts p
+            where p.parent <> 0'
         );
-        $ex->exportTable('Comment', "
-         select p.*,
-          case p.editedby when 0 then null else p.editedby end as editedby2
-         from :_posts p
-         where p.parent <> 0
-        ", $comment_Map);
 
         // UserDiscussion.
         // This is the table for assigning bookmarks/subscribed threads.
@@ -260,7 +243,6 @@ class MODXDiscuss extends ExportController {
 
         $ex->endExport();
     }
-
 }
 
 // Closing PHP tag required. (make.php)
