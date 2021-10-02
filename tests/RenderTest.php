6522a1069c0c4265c4c06e61be48716f925f4c36
Lincoln Russell <lincoln@icrontic.com>
2021-10-02 10:48:41 -0400

    Add first running test

---
+++ tests/RenderTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class RenderTest extends TestCase
+{
+    public function testCanBeUsedAsString(): void
+    {
+        ob_start();
+        pageFooter();
+        $output = ob_get_clean();
+        $this->assertStringContainsString('</html>', $output);
+    }
+}
