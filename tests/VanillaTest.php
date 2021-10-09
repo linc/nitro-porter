be94161bceffff208699b9bb976f95b3caa57de1
Lincoln Russell <lincoln@icrontic.com>
2021-10-09 12:58:38 -0400

    Add example of selective seeding

--- tests/VanillaTest.php
+++ tests/VanillaTest.php
@@ -3,6 +3,8 @@
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Console\Input\StringInput;
 use Symfony\Component\Console\Output\NullOutput;
+use Phinx\Config\Config;
+use Phinx\Migration\Manager;
 
 class VanillaTest extends TestCase
 {
@@ -12,5 +14,19 @@ class VanillaTest extends TestCase
         $app->setAutoExit(false);
         $app->run(new StringInput('migrate'), new NullOutput());
         $app->run(new StringInput('seed:run'), new NullOutput());
+    }
+
+    public function seed(): void
+    {
+        $configArray = require('phinx.php');
+        $config = new Config($configArray);
+        $manager = new Manager($config, new StringInput(' '), new NullOutput());
+        $manager->seed('testing');
+    }
+
+    public function testItSeedsDatabaseLol(): bool
+    {
+        //$this->seed();
+        $this->markTestIncomplete('Not written yet.');
     }
 }
