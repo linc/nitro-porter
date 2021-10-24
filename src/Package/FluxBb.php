13b2861379264703cc4226d758c45dc9f28880aa
Lincoln Russell <lincoln@icrontic.com>
2021-10-24 17:04:43 -0400

    Refactor FluxBB to separate methods

--- src/Package/FluxBb.php
+++ src/Package/FluxBb.php
@@ -14,7 +14,6 @@ use NitroPorter\ExportModel;
 
 class FluxBb extends ExportController
 {
-
     public const SUPPORTED = [
         'name' => 'FluxBB 1',
         'prefix' => '',
@@ -72,7 +71,6 @@ class FluxBb extends ExportController
      */
     protected function forumExport($ex)
     {
-
         $characterSet = $ex->getCharacterSet('posts');
         if ($characterSet) {
             $ex->characterSet = $characterSet;
@@ -90,196 +88,21 @@ class FluxBb extends ExportController
             }
         }
 
-        // User
-        $user_Map = array(
-            'AvatarID' => array('Column' => 'Photo', 'Filter' => array($this, 'getAvatarByID')),
-        );
-        $ex->exportTable(
-            'User',
-            "
-            select
-                u.id as UserID,
-                u.username as UserID,
-                u.email as Email,
-                u.timezone as HourOffset,
-                u.registration_ip as InsertIPAddress,
-                u.id as AvatarID,
-                u.password as Password,
-                from_unixtime(u.registered) as DateInserted,
-                from_unixtime(u.last_visit) as DateLastActive
-            from :_users u
-            where group_id <> 2",
-            $user_Map
-        );
+        $this->users($ex);
 
-        // Role
-        $ex->exportTable(
-            'Role',
-            "
-            select
-                g_id as RoleID,
-                 g_title as Name
-            from :_groups"
-        );
+        $this->permissions($ex);
+        $this->roles($ex);
+        $this->signatures($ex);
 
-        // Permission
+        $this->categories($ex);
 
-        $ex->exportTable(
-            'Permission',
-            "
-            select
-                g_id,
-                g_moderator as 'Garden.Moderation.Manage',
-                g_mod_edit_users as 'Garden.Users.Edit',
-                g_mod_rename_users as 'Garden.Users.Delete',
-                g_read_board as 'Vanilla.Discussions.View',
-                g_view_users as 'Garden.Profiles.View',
-                g_post_topics as 'Vanilla.Discussions.Add',
-                g_post_replies as 'Vanilla.Comments.Add',
-                g_post_replies as 'Garden.SignIn.Allow',
-                g_mod_edit_users as 'Garden.Users.Add',
-                case
-                    when g_title = 'Administrators' then 'All' else NULL
-                end as _Permissions
-            from :_groups"
-        );
+        $this->discussions($ex);
 
-        // UserRole
-        $ex->exportTable(
-            'UserRole',
-            "
-            select
-                u.id as UserID,
-                u.group_id as RoleID
-            from :_users u"
-        );
+        $this->comments($ex);
 
-        // Signatures.
-        $ex->exportTable(
-            'UserMeta',
-            "
-            select
-                u.id as UserID,
-                'Plugin.Signatures.Sig' as Name,
-                signature as Value
-            from :_users u
-            where u.signature is not null"
-        );
+        $this->tags($ex);
+        $this->attachments($ex);
 
-        // Category
-        $ex->exportTable(
-            'Category',
-            "
-            select
-                id as CategoryID,
-                forum_name as Name,
-                forum_desc as Description,
-                disp_position as Sort,
-                cat_id * 1000 as ParentCategoryID
-            from :_forums f
-
-            union
-            select
-                id * 1000 as CategoryID,
-                cat_name as Name,
-                '' as Description,
-                disp_position as Sort,
-                NULL as ParentCategoryID
-            from :_categories"
-        );
-
-        // Discussion.
-        $ex->exportTable(
-            'Discussion',
-            "
-            select
-                t.id as DiscussionID,
-                from_unixtime(p.posted) as DateInserted,
-                p.poster_id as InsertUserID,
-                p.poster_ip as InsertIPAddress,
-                p.message as Body,
-                t.closed as Closed,
-                t.sticky as Announce,
-                t.forum_id as CategoryID,
-                t.subject as Name,
-                from_unixtime(p.edited) as DateUpdated,
-                u.id as UpdateUserID,
-                'BBCode' as Format
-            from :_topics t
-            left join :_posts p on t.first_post_id = p.id
-            left join :_users u on u.username = p.edited_by"
-        );
-
-        // Comment.
-        $ex->exportTable(
-            'Comment',
-            "
-            select
-                p.*,
-                p.id as CommentID,
-                p.poster_id as InsertUserID,
-                p.poster_ip as InsertIPAddress,
-                p.message as Body,
-                'BBCode' as Format,
-                from_unixtime(p.posted) as DateInserted,
-                from_unixtime(p.edited) as DateUpdated,
-                u.id as UpdateUserID
-            from :_topics t
-            join :_posts p on t.id = p.topic_id
-            left join :_users u on u.username = p.edited_by
-            where p.id <> t.first_post_id;"
-        );
-
-        if ($ex->exists('tags')) {
-            // Tag.
-            $ex->exportTable(
-                'Tag',
-                "
-                select
-                    id as TagID,
-                     tag as Name
-                from :_tags"
-            );
-
-            // TagDisucssion.
-            $ex->exportTable(
-                'TagDiscussion',
-                "
-                select
-                    topic_id as DiscussionID,
-                     tag_id as TagID
-                from :_topic_tags"
-            );
-        }
-
-        if ($ex->exists('attach_files')) {
-            // Media.
-            $media_Map = array(
-                'owner_id' => 'InsertUserID',
-                'thumb_path' => array('Column' => 'ThumbPath', 'Filter' => array($this, 'filterThumbnailData')),
-                'thumb_width' => array('Column' => 'ThumbWidth', 'Filter' => array($this, 'filterThumbnailData')),
-            );
-            $ex->exportTable(
-                'Media',
-                "
-                select f.*,
-                    f.id as MediaID,
-                    f.filename as Name,
-                    f.size as Size,
-                    f.type as Type,
-                    f.ownder_id as InsertUserID,
-                    concat({$this->cdn}, 'FileUpload/', f.file_path) as Path,
-                    concat({$this->cdn}, 'FileUpload/', f.file_path) as thumb_path,
-                    128 as thumb_width,
-                    from_unixtime(f.uploaded_at) as DateInserted,
-                    case when f.post_id is null then 'Discussion' else 'Comment' end as ForeignTable,
-                    coalesce(f.post_id, f.topic_id) as ForeignID
-                from :_attach_files f",
-                $media_Map
-            );
-        }
-
-        // End
         $ex->endExport();
     }
 
@@ -340,6 +163,240 @@ class FluxBb extends ExportController
             return $value;
         } else {
             return null;
+        }
+    }
+
+    /**
+     * @param ExportModel $ex
+     */
+    protected function users(ExportModel $ex): void
+    {
+        $user_Map = array(
+            'AvatarID' => array('Column' => 'Photo', 'Filter' => array($this, 'getAvatarByID')),
+        );
+        $ex->exportTable(
+            'User',
+            "
+            select
+                u.id as UserID,
+                u.username as UserID,
+                u.email as Email,
+                u.timezone as HourOffset,
+                u.registration_ip as InsertIPAddress,
+                u.id as AvatarID,
+                u.password as Password,
+                from_unixtime(u.registered) as DateInserted,
+                from_unixtime(u.last_visit) as DateLastActive
+            from :_users u
+            where group_id <> 2",
+            $user_Map
+        );
+    }
+
+    /**
+     * @param ExportModel $ex
+     */
+    protected function permissions(ExportModel $ex): void
+    {
+        $ex->exportTable(
+            'Permission',
+            "
+            select
+                g_id,
+                g_moderator as 'Garden.Moderation.Manage',
+                g_mod_edit_users as 'Garden.Users.Edit',
+                g_mod_rename_users as 'Garden.Users.Delete',
+                g_read_board as 'Vanilla.Discussions.View',
+                g_view_users as 'Garden.Profiles.View',
+                g_post_topics as 'Vanilla.Discussions.Add',
+                g_post_replies as 'Vanilla.Comments.Add',
+                g_post_replies as 'Garden.SignIn.Allow',
+                g_mod_edit_users as 'Garden.Users.Add',
+                case
+                    when g_title = 'Administrators' then 'All' else NULL
+                end as _Permissions
+            from :_groups"
+        );
+    }
+
+    /**
+     * @param ExportModel $ex
+     */
+    protected function roles(ExportModel $ex): void
+    {
+        $ex->exportTable(
+            'Role',
+            "
+            select
+                g_id as RoleID,
+                 g_title as Name
+            from :_groups"
+        );
+        // UserRole
+        $ex->exportTable(
+            'UserRole',
+            "
+            select
+                u.id as UserID,
+                u.group_id as RoleID
+            from :_users u"
+        );
+    }
+
+    /**
+     * @param ExportModel $ex
+     */
+    protected function signatures(ExportModel $ex): void
+    {
+        $ex->exportTable(
+            'UserMeta',
+            "
+            select
+                u.id as UserID,
+                'Plugin.Signatures.Sig' as Name,
+                signature as Value
+            from :_users u
+            where u.signature is not null"
+        );
+    }
+
+    /**
+     * @param ExportModel $ex
+     */
+    protected function categories(ExportModel $ex): void
+    {
+        $ex->exportTable(
+            'Category',
+            "
+            select
+                id as CategoryID,
+                forum_name as Name,
+                forum_desc as Description,
+                disp_position as Sort,
+                cat_id * 1000 as ParentCategoryID
+            from :_forums f
+
+            union
+            select
+                id * 1000 as CategoryID,
+                cat_name as Name,
+                '' as Description,
+                disp_position as Sort,
+                NULL as ParentCategoryID
+            from :_categories"
+        );
+    }
+
+    /**
+     * @param ExportModel $ex
+     */
+    protected function discussions(ExportModel $ex): void
+    {
+        $ex->exportTable(
+            'Discussion',
+            "
+            select
+                t.id as DiscussionID,
+                from_unixtime(p.posted) as DateInserted,
+                p.poster_id as InsertUserID,
+                p.poster_ip as InsertIPAddress,
+                p.message as Body,
+                t.closed as Closed,
+                t.sticky as Announce,
+                t.forum_id as CategoryID,
+                t.subject as Name,
+                from_unixtime(p.edited) as DateUpdated,
+                u.id as UpdateUserID,
+                'BBCode' as Format
+            from :_topics t
+            left join :_posts p on t.first_post_id = p.id
+            left join :_users u on u.username = p.edited_by"
+        );
+    }
+
+    /**
+     * @param ExportModel $ex
+     */
+    protected function comments(ExportModel $ex): void
+    {
+        $ex->exportTable(
+            'Comment',
+            "
+            select
+                p.*,
+                p.id as CommentID,
+                p.poster_id as InsertUserID,
+                p.poster_ip as InsertIPAddress,
+                p.message as Body,
+                'BBCode' as Format,
+                from_unixtime(p.posted) as DateInserted,
+                from_unixtime(p.edited) as DateUpdated,
+                u.id as UpdateUserID
+            from :_topics t
+            join :_posts p on t.id = p.topic_id
+            left join :_users u on u.username = p.edited_by
+            where p.id <> t.first_post_id;"
+        );
+    }
+
+    /**
+     * @param ExportModel $ex
+     */
+    protected function tags(ExportModel $ex): void
+    {
+        if ($ex->exists('tags')) {
+            // Tag.
+            $ex->exportTable(
+                'Tag',
+                "
+                select
+                    id as TagID,
+                     tag as Name
+                from :_tags"
+            );
+
+            // TagDisucssion.
+            $ex->exportTable(
+                'TagDiscussion',
+                "
+                select
+                    topic_id as DiscussionID,
+                     tag_id as TagID
+                from :_topic_tags"
+            );
+        }
+    }
+
+    /**
+     * @param ExportModel $ex
+     */
+    protected function attachments(ExportModel $ex): void
+    {
+        if ($ex->exists('attach_files')) {
+            // Media.
+            $media_Map = array(
+                'owner_id' => 'InsertUserID',
+                'thumb_path' => array('Column' => 'ThumbPath', 'Filter' => array($this, 'filterThumbnailData')),
+                'thumb_width' => array('Column' => 'ThumbWidth', 'Filter' => array($this, 'filterThumbnailData')),
+            );
+            $ex->exportTable(
+                'Media',
+                "
+                select f.*,
+                    f.id as MediaID,
+                    f.filename as Name,
+                    f.size as Size,
+                    f.type as Type,
+                    f.ownder_id as InsertUserID,
+                    concat({$this->cdn}, 'FileUpload/', f.file_path) as Path,
+                    concat({$this->cdn}, 'FileUpload/', f.file_path) as thumb_path,
+                    128 as thumb_width,
+                    from_unixtime(f.uploaded_at) as DateInserted,
+                    case when f.post_id is null then 'Discussion' else 'Comment' end as ForeignTable,
+                    coalesce(f.post_id, f.topic_id) as ForeignID
+                from :_attach_files f",
+                $media_Map
+            );
         }
     }
 }
