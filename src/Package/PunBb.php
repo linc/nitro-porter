e07ccfec53b90ad3e14ffd8ba6bdd42efc1eeb90
Lincoln Russell <lincoln@icrontic.com>
2021-10-24 15:02:54 -0400

    Refactor PunBB to separate methods

--- src/Package/PunBb.php
+++ src/Package/PunBb.php
@@ -14,7 +14,6 @@ use NitroPorter\ExportModel;
 
 class PunBb extends ExportController
 {
-
     public const SUPPORTED = [
         'name' => 'PunBB 1',
         'prefix' => 'punbb_',
@@ -69,7 +68,6 @@ class PunBb extends ExportController
      */
     protected function forumExport($ex)
     {
-
         $characterSet = $ex->getCharacterSet('posts');
         if ($characterSet) {
             $ex->characterSet = $characterSet;
@@ -89,238 +87,21 @@ class PunBb extends ExportController
         }
         unset($avatarPath);
 
-        // User.
-        $user_Map = array(
-            'AvatarID' => array('Column' => 'Photo', 'Filter' => array($this, 'getAvatarByID')),
-            'id' => 'UserID',
-            'username' => 'Name',
-            'email' => 'Email',
-            'timezone' => 'HourOffset',
-            'registration_ip' => 'InsertIPAddress',
-            'PasswordHash' => 'Password'
-        );
-        $ex->exportTable(
-            'User',
-            "
-         SELECT
-             u.*, u.id AS AvatarID,
-             concat(u.password, '$', u.salt) AS PasswordHash,
-             from_unixtime(registered) AS DateInserted,
-             from_unixtime(last_visit) AS DateLastActive
-         FROM :_users u
-         WHERE group_id <> 2",
-            $user_Map
-        );
+        $this->users($ex);
 
-        // Role.
-        $role_Map = array(
-            'g_id' => 'RoleID',
-            'g_title' => 'Name'
-        );
-        $ex->exportTable('Role', "SELECT * FROM :_groups", $role_Map);
+        $this->roles($ex);
 
-        // Permission.
-        $permission_Map = array(
-            'g_id' => 'RoleID',
-            'g_modertor' => 'Garden.Moderation.Manage',
-            'g_mod_edit_users' => 'Garden.Users.Edit',
-            'g_mod_rename_users' => 'Garden.Users.Delete',
-            'g_read_board' => 'Vanilla.Discussions.View',
-            'g_view_users' => 'Garden.Profiles.View',
-            'g_post_topics' => 'Vanilla.Discussions.Add',
-            'g_post_replies' => 'Vanilla.Comments.Add',
-            'g_pun_attachment_allow_download' => 'Plugins.Attachments.Download.Allow',
-            'g_pun_attachment_allow_upload' => 'Plugins.Attachments.Upload.Allow',
+        $this->permissions($ex);
+        $this->signatures($ex);
 
-        );
-        $permission_Map = $ex->fixPermissionColumns($permission_Map);
-        $ex->exportTable(
-            'Permission',
-            "
-      SELECT
-         g.*,
-         g_post_replies AS `Garden.SignIn.Allow`,
-         g_mod_edit_users AS `Garden.Users.Add`,
-         CASE WHEN g_title = 'Administrators' THEN 'All' ELSE NULL END AS _Permissions
-      FROM :_groups g",
-            $permission_Map
-        );
+        $this->categories($ex);
 
-        // UserRole.
-        $userRole_Map = array(
-            'id' => 'UserID',
-            'group_id' => 'RoleID'
-        );
-        $ex->exportTable(
-            'UserRole',
-            "SELECT
-            CASE u.group_id WHEN 2 THEN 0 ELSE id END AS id,
-            u.group_id
-          FROM :_users u",
-            $userRole_Map
-        );
+        $this->discussions($ex);
 
-        // Signatures.
-        $ex->exportTable(
-            'UserMeta',
-            "
-        select
-           u.id as UserID,
-           'Plugin.Signatures.Format' AS Name,
-           'BBCode' as Value
-        from
-           :_users u
-        where
-            u.signature is not null
-        and
-            u.signature != ''
+        $this->comments($ex);
+        $this->tags($ex);
+        $this->attachments($ex);
 
-        union
-
-        select
-            u.id as UserID,
-            'Plugin.Signatures.Sig' AS Name,
-            signature as Value
-        from
-            :_users u
-        where
-            u.signature is not null
-        and
-            u.signature !=''
-        "
-        );
-
-
-        // Category.
-        $category_Map = array(
-            'id' => 'CategoryID',
-            'forum_name' => 'Name',
-            'forum_desc' => 'Description',
-            'disp_position' => 'Sort',
-            'parent_id' => 'ParentCategoryID'
-        );
-        $ex->exportTable(
-            'Category',
-            "
-      SELECT
-        id,
-        forum_name,
-        forum_desc,
-        disp_position,
-        cat_id * 1000 AS parent_id
-      FROM :_forums f
-      UNION
-
-      SELECT
-        id * 1000,
-        cat_name,
-        '',
-        disp_position,
-        NULL
-      FROM :_categories",
-            $category_Map
-        );
-
-        // Discussion.
-        $discussion_Map = array(
-            'id' => 'DiscussionID',
-            'poster_id' => 'InsertUserID',
-            'poster_ip' => 'InsertIPAddress',
-            'closed' => 'Closed',
-            'sticky' => 'Announce',
-            'forum_id' => 'CategoryID',
-            'subject' => 'Name',
-            'message' => 'Body'
-
-        );
-        $ex->exportTable(
-            'Discussion',
-            "
-      SELECT t.*,
-        from_unixtime(p.posted) AS DateInserted,
-        p.poster_id,
-        p.poster_ip,
-        p.message,
-        from_unixtime(p.edited) AS DateUpdated,
-        eu.id AS UpdateUserID,
-        'BBCode' AS Format
-      FROM :_topics t
-      LEFT JOIN :_posts p
-        ON t.first_post_id = p.id
-      LEFT JOIN :_users eu
-        ON eu.username = p.edited_by",
-            $discussion_Map
-        );
-
-        // Comment.
-        $comment_Map = array(
-            'id' => 'CommentID',
-            'topic_id' => 'DiscussionID',
-            'poster_id' => 'InsertUserID',
-            'poster_ip' => 'InsertIPAddress',
-            'message' => 'Body'
-        );
-        $ex->exportTable(
-            'Comment',
-            "
-            SELECT p.*,
-        'BBCode' AS Format,
-        from_unixtime(p.posted) AS DateInserted,
-        from_unixtime(p.edited) AS DateUpdated,
-        eu.id AS UpdateUserID
-      FROM :_topics t
-      JOIN :_posts p
-        ON t.id = p.topic_id
-      LEFT JOIN :_users eu
-        ON eu.username = p.edited_by
-      WHERE p.id <> t.first_post_id;",
-            $comment_Map
-        );
-
-        if ($ex->exists('tags')) {
-            // Tag.
-            $tag_Map = array(
-                'id' => 'TagID',
-                'tag' => 'Name'
-            );
-            $ex->exportTable('Tag', "SELECT * FROM :_tags", $tag_Map);
-
-            // TagDisucssion.
-            $tagDiscussionMap = array(
-                'topic_id' => 'DiscussionID',
-                'tag_id' => 'TagID'
-            );
-            $ex->exportTable('TagDiscussion', "SELECT * FROM :_topic_tags", $tagDiscussionMap);
-        }
-
-        if ($ex->exists('attach_files')) {
-            // Media.
-            $media_Map = array(
-                'id' => 'MediaID',
-                'filename' => 'Name',
-                'file_mime_type' => 'Type',
-                'size' => 'Size',
-                'owner_id' => 'InsertUserID',
-                'thumb_path' => array('Column' => 'ThumbPath', 'Filter' => array($this, 'filterThumbnailData')),
-                'thumb_width' => array('Column' => 'ThumbWidth', 'Filter' => array($this, 'filterThumbnailData')),
-            );
-            $ex->exportTable(
-                'Media',
-                "
-                select f.*,
-                    concat({$this->cdn}, 'FileUpload/', f.file_path) as Path,
-                    concat({$this->cdn}, 'FileUpload/', f.file_path) as thumb_path,
-                    128 as thumb_width,
-                    from_unixtime(f.uploaded_at) as DateInserted,
-                    case when post_id is null then 'Discussion' else 'Comment' end as ForeignTable,
-                    coalesce(post_id, topic_id) as ForieignID
-                from :_attach_files f
-            ",
-                $media_Map
-            );
-        }
-
-        // End
         $ex->endExport();
     }
 
@@ -382,5 +163,282 @@ class PunBb extends ExportController
         } else {
             return null;
         }
+    }
+
+    /**
+     * @param ExportModel $ex
+     */
+    protected function attachments(ExportModel $ex): void
+    {
+        if ($ex->exists('attach_files')) {
+            // Media.
+            $media_Map = array(
+                'id' => 'MediaID',
+                'filename' => 'Name',
+                'file_mime_type' => 'Type',
+                'size' => 'Size',
+                'owner_id' => 'InsertUserID',
+                'thumb_path' => array('Column' => 'ThumbPath', 'Filter' => array($this, 'filterThumbnailData')),
+                'thumb_width' => array('Column' => 'ThumbWidth', 'Filter' => array($this, 'filterThumbnailData')),
+            );
+            $ex->exportTable(
+                'Media',
+                "
+                select f.*,
+                    concat({$this->cdn}, 'FileUpload/', f.file_path) as Path,
+                    concat({$this->cdn}, 'FileUpload/', f.file_path) as thumb_path,
+                    128 as thumb_width,
+                    from_unixtime(f.uploaded_at) as DateInserted,
+                    case when post_id is null then 'Discussion' else 'Comment' end as ForeignTable,
+                    coalesce(post_id, topic_id) as ForieignID
+                from :_attach_files f
+            ",
+                $media_Map
+            );
+        }
+    }
+
+    /**
+     * @param ExportModel $ex
+     */
+    protected function tags(ExportModel $ex): void
+    {
+        if ($ex->exists('tags')) {
+            // Tag.
+            $tag_Map = array(
+                'id' => 'TagID',
+                'tag' => 'Name'
+            );
+            $ex->exportTable('Tag', "SELECT * FROM :_tags", $tag_Map);
+
+            // TagDisucssion.
+            $tagDiscussionMap = array(
+                'topic_id' => 'DiscussionID',
+                'tag_id' => 'TagID'
+            );
+            $ex->exportTable('TagDiscussion', "SELECT * FROM :_topic_tags", $tagDiscussionMap);
+        }
+    }
+
+    /**
+     * @param ExportModel $ex
+     */
+    protected function comments(ExportModel $ex): void
+    {
+        $comment_Map = array(
+            'id' => 'CommentID',
+            'topic_id' => 'DiscussionID',
+            'poster_id' => 'InsertUserID',
+            'poster_ip' => 'InsertIPAddress',
+            'message' => 'Body'
+        );
+        $ex->exportTable(
+            'Comment',
+            "
+            SELECT p.*,
+        'BBCode' AS Format,
+        from_unixtime(p.posted) AS DateInserted,
+        from_unixtime(p.edited) AS DateUpdated,
+        eu.id AS UpdateUserID
+      FROM :_topics t
+      JOIN :_posts p
+        ON t.id = p.topic_id
+      LEFT JOIN :_users eu
+        ON eu.username = p.edited_by
+      WHERE p.id <> t.first_post_id;",
+            $comment_Map
+        );
+    }
+
+    /**
+     * @param ExportModel $ex
+     */
+    protected function discussions(ExportModel $ex): void
+    {
+        $discussion_Map = array(
+            'id' => 'DiscussionID',
+            'poster_id' => 'InsertUserID',
+            'poster_ip' => 'InsertIPAddress',
+            'closed' => 'Closed',
+            'sticky' => 'Announce',
+            'forum_id' => 'CategoryID',
+            'subject' => 'Name',
+            'message' => 'Body'
+
+        );
+        $ex->exportTable(
+            'Discussion',
+            "
+      SELECT t.*,
+        from_unixtime(p.posted) AS DateInserted,
+        p.poster_id,
+        p.poster_ip,
+        p.message,
+        from_unixtime(p.edited) AS DateUpdated,
+        eu.id AS UpdateUserID,
+        'BBCode' AS Format
+      FROM :_topics t
+      LEFT JOIN :_posts p
+        ON t.first_post_id = p.id
+      LEFT JOIN :_users eu
+        ON eu.username = p.edited_by",
+            $discussion_Map
+        );
+    }
+
+    /**
+     * @param ExportModel $ex
+     */
+    protected function categories(ExportModel $ex): void
+    {
+        $category_Map = array(
+            'id' => 'CategoryID',
+            'forum_name' => 'Name',
+            'forum_desc' => 'Description',
+            'disp_position' => 'Sort',
+            'parent_id' => 'ParentCategoryID'
+        );
+        $ex->exportTable(
+            'Category',
+            "
+      SELECT
+        id,
+        forum_name,
+        forum_desc,
+        disp_position,
+        cat_id * 1000 AS parent_id
+      FROM :_forums f
+      UNION
+
+      SELECT
+        id * 1000,
+        cat_name,
+        '',
+        disp_position,
+        NULL
+      FROM :_categories",
+            $category_Map
+        );
+    }
+
+    /**
+     * @param ExportModel $ex
+     */
+    protected function signatures(ExportModel $ex): void
+    {
+        $ex->exportTable(
+            'UserMeta',
+            "
+        select
+           u.id as UserID,
+           'Plugin.Signatures.Format' AS Name,
+           'BBCode' as Value
+        from
+           :_users u
+        where
+            u.signature is not null
+        and
+            u.signature != ''
+
+        union
+
+        select
+            u.id as UserID,
+            'Plugin.Signatures.Sig' AS Name,
+            signature as Value
+        from
+            :_users u
+        where
+            u.signature is not null
+        and
+            u.signature !=''
+        "
+        );
+    }
+
+    /**
+     * @param ExportModel $ex
+     */
+    protected function roles(ExportModel $ex): void
+    {
+        $role_Map = array(
+            'g_id' => 'RoleID',
+            'g_title' => 'Name'
+        );
+        $ex->exportTable('Role', "SELECT * FROM :_groups", $role_Map);
+
+        // UserRole.
+        $userRole_Map = array(
+            'id' => 'UserID',
+            'group_id' => 'RoleID'
+        );
+        $ex->exportTable(
+            'UserRole',
+            "SELECT
+            CASE u.group_id WHEN 2 THEN 0 ELSE id END AS id,
+            u.group_id
+          FROM :_users u",
+            $userRole_Map
+        );
+    }
+
+    /**
+     * @param ExportModel $ex
+     */
+    protected function permissions(ExportModel $ex): void
+    {
+        $permission_Map = array(
+            'g_id' => 'RoleID',
+            'g_modertor' => 'Garden.Moderation.Manage',
+            'g_mod_edit_users' => 'Garden.Users.Edit',
+            'g_mod_rename_users' => 'Garden.Users.Delete',
+            'g_read_board' => 'Vanilla.Discussions.View',
+            'g_view_users' => 'Garden.Profiles.View',
+            'g_post_topics' => 'Vanilla.Discussions.Add',
+            'g_post_replies' => 'Vanilla.Comments.Add',
+            'g_pun_attachment_allow_download' => 'Plugins.Attachments.Download.Allow',
+            'g_pun_attachment_allow_upload' => 'Plugins.Attachments.Upload.Allow',
+
+        );
+        $permission_Map = $ex->fixPermissionColumns($permission_Map);
+        $ex->exportTable(
+            'Permission',
+            "
+      SELECT
+         g.*,
+         g_post_replies AS `Garden.SignIn.Allow`,
+         g_mod_edit_users AS `Garden.Users.Add`,
+         CASE WHEN g_title = 'Administrators' THEN 'All' ELSE NULL END AS _Permissions
+      FROM :_groups g",
+            $permission_Map
+        );
+    }
+
+    /**
+     * @param ExportModel $ex
+     */
+    protected function users(ExportModel $ex): void
+    {
+        $user_Map = array(
+            'AvatarID' => array('Column' => 'Photo', 'Filter' => array($this, 'getAvatarByID')),
+            'id' => 'UserID',
+            'username' => 'Name',
+            'email' => 'Email',
+            'timezone' => 'HourOffset',
+            'registration_ip' => 'InsertIPAddress',
+            'PasswordHash' => 'Password'
+        );
+        $ex->exportTable(
+            'User',
+            "
+         SELECT
+             u.*, u.id AS AvatarID,
+             concat(u.password, '$', u.salt) AS PasswordHash,
+             from_unixtime(registered) AS DateInserted,
+             from_unixtime(last_visit) AS DateLastActive
+         FROM :_users u
+         WHERE group_id <> 2",
+            $user_Map
+        );
     }
 }
