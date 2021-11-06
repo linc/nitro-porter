8a46e9e1e80cfb1147afcdd7a6d655163b19460f
Lincoln Russell <lincoln@icrontic.com>
2021-11-06 14:53:53 -0400

    Simplify the bootstrap more

--- index.php
+++ index.php
@@ -4,28 +4,22 @@
  * @license http://opensource.org/licenses/gpl-2.0.php GNU GPL2
  */
 
+// Autoload.
+require_once 'vendor/autoload.php';
+if (!file_exists(__DIR__ . '/config.php')) {
+    die('Required file config.php missing');
+}
+
 // Environment.
 const VERSION = '2.5';
 const ROOT_DIR = __DIR__;
 const DB_EXTENSION = 'pdo'; // @todo
-
-if (!file_exists(ROOT_DIR . '/config.php')) {
-    // Require config.
-    die('Required file config.php missing');
-}
-
 error_reporting(E_ERROR | E_PARSE | E_CORE_ERROR | E_COMPILE_ERROR | E_USER_ERROR | E_RECOVERABLE_ERROR);
 ini_set('display_errors', 'on');
 ini_set('track_errors', 1);
 if (ini_get('date.timezone') == '') {
-    // Set default time zone.
     date_default_timezone_set('America/Detroit');
 }
-
-// Autoload.
-require_once 'vendor/autoload.php';
-
-// Bootstrap.
 set_error_handler("ErrorHandler");
 
 // CLI Router.
