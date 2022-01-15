fef7bee5364439d2659e0445f1ccd018f0fb9ae6
Lincoln Russell <lincoln@icrontic.com>
2022-01-15 12:38:02 -0500

    Separate conf check from autoload

--- index.php
+++ index.php
@@ -6,15 +6,17 @@
 
 // Autoload.
 require_once 'vendor/autoload.php';
-if (!file_exists(__DIR__ . '/config.php')) {
-    die('Required file config.php missing');
-}
 
 // Environment.
 const ROOT_DIR = __DIR__;
 set_error_handler("ErrorHandler");
 if (ini_get('date.timezone') == '') {
     date_default_timezone_set('America/Detroit');
+}
+
+// Require config.
+if (!file_exists(__DIR__ . '/config.php')) {
+    die('Required file config.php missing');
 }
 
 // CLI Router.
