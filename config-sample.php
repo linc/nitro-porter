3af88d6f9e88b8d22378176c3748751900bb1e03
Lincoln Russell <lincoln@icrontic.com>
2022-01-14 20:59:08 -0500

    Fix config array nesting

--- config-sample.php
+++ config-sample.php
@@ -4,26 +4,30 @@ return [
     'debug' => false,
     'connections' => [
         'databases' => [
-            'alias' => 'First Database',
-            'adapter' => 'mysql',
-            'host' => 'localhost',
-            'port' => 3306,
-            'name' => 'some_db_name',
-            'user' => 'root',
-            'pass' => 'password_here',
-            'charset' => 'utf8',
+            [
+                'alias' => 'First Database',
+                'adapter' => 'mysql',
+                'host' => 'localhost',
+                'port' => 3306,
+                'name' => 'some_db_name',
+                'user' => 'root',
+                'pass' => 'password_here',
+                'charset' => 'utf8',
+            ],
         ],
     ],
     'test_connections' => [
        'databases' => [
-            'alias' => 'Test DB',
-            'adapter' => 'mysql',
-            'host' => '127.0.0.1',
-            'port' => '3306',
-            'name' => 'testing_db',
-            'user' => 'root',
-            'pass' => 'password_here',
-            'charset' => 'utf8',
-        ]
-    ]
+           [
+                'alias' => 'Test DB',
+                'adapter' => 'mysql',
+                'host' => '127.0.0.1',
+                'port' => '3306',
+                'name' => 'testing_db',
+                'user' => 'root',
+                'pass' => 'password_here',
+                'charset' => 'utf8',
+           ],
+        ],
+    ],
 ];
