6c769e7c2a602509684026489ece6702f0d73e2b
Lincoln Russell <lincoln@icrontic.com>
2022-01-22 15:33:14 -0600

    Extract the bootstrap

---
+++ bootstrap.php
@@ -0,0 +1,23 @@
+<?php
+
+if (!defined('WEB') && !defined('CONSOLE')) {
+    die('Cannot call bootstrap directly.');
+}
+
+// Autoload.
+require_once 'vendor/autoload.php';
+
+// Environment.
+const ROOT_DIR = __DIR__;
+set_error_handler("ErrorHandler");
+if (ini_get('date.timezone') == '') {
+    date_default_timezone_set('America/Detroit');
+}
+
+// Require config.
+if (!file_exists(__DIR__ . '/config.php')) {
+    die('Required file config.php missing');
+}
+
+// Load package manifest.
+\NitroPorter\PackageSupport::getInstance()->set(loadManifest());
--- index.php
+++ index.php
@@ -1,33 +1,8 @@
 <?php
 
-/**
- * @license http://opensource.org/licenses/gpl-2.0.php GNU GPL2
- */
-
-// Autoload.
-require_once 'vendor/autoload.php';
-
-// Environment.
-const ROOT_DIR = __DIR__;
-set_error_handler("ErrorHandler");
-if (ini_get('date.timezone') == '') {
-    date_default_timezone_set('America/Detroit');
-}
-
-// Require config.
-if (!file_exists(__DIR__ . '/config.php')) {
-    die('Required file config.php missing');
-}
-
-// Load the support manifest.
-\NitroPorter\PackageSupport::getInstance()->set(loadManifest());
-
-// CLI Router.
-if (PHP_SAPI == 'cli') {
-    define('CONSOLE', true);
-    $cli = new \NitroPorter\CommandLine();
-    $cli->parse();
-}
+// Bootstrap.
+define('WEB', true);
+require_once './bootstrap.php';
 
 // Web Router.
 if (isset($_POST['package'])) {
@@ -35,9 +10,4 @@ if (isset($_POST['package'])) {
     $package->run();
 } else {
     \NitroPorter\Render::route();
-}
-
-if (defined('CONSOLE')) {
-    // Console output should end in newline.
-    echo "\n";
 }
