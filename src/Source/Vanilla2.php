e27be226447e774d13ed2156f73ee2abcd71fc44
Lincoln Russell <lincoln@icrontic.com>
2022-04-01 19:20:37 -0400

    Add Yaga support to Vanilla export

--- src/Source/Vanilla2.php
+++ src/Source/Vanilla2.php
@@ -39,7 +39,6 @@ class Vanilla2 extends Source
             'Groups' => 0,
             'Tags' => 1,
             'Reactions' => 1,
-            'Articles' => 0,
         ]
     ];
 
@@ -51,11 +50,11 @@ class Vanilla2 extends Source
     /**
      * @param ExportModel $ex
      */
-    public function run($ex)
+    public function run(ExportModel $ex)
     {
-        $tables = array(
+        // Core tables essentially map to our intermediate format as-is.
+        $tables = [
             //'Activity',
-            'Badge', // SaaS-only
             'Category',
             'Comment',
             'Conversation',
@@ -63,27 +62,121 @@ class Vanilla2 extends Source
             'Discussion',
             'Media',
             //'Permission',
-            'Poll', // SaaS-only
-            'PollOption', // SaaS-only
-            'PollVote', // SaaS-only
-            'ReactionType',
-            'Rank', // SaaS-only
             'Role',
             'Tag',
             'TagDiscussion',
             'User',
-            'UserBadge', // SaaS-only
             'UserComment',
             'UserConversation',
             'UserDiscussion',
             'UserMeta',
-            'UserRole'
-        );
-
+            'UserRole',
+        ];
         foreach ($tables as $tableName) {
             if ($ex->exists($tableName)) {
                 $ex->export($tableName, "select * from :_{$tableName}");
             }
+        }
+
+        $this->badges($ex);
+        $this->ranks($ex);
+        $this->reactions($ex);
+        $this->polls($ex);
+    }
+
+    /**
+     * Badges support for cloud + Yaga.
+     *
+     * @param ExportModel $ex
+     */
+    public function badges(ExportModel $ex)
+    {
+        if ($ex->exists('Badge')) {
+            // Vanilla Cloud
+            $ex->export('Badge', "select * from :_Badge");
+            $ex->export('UserBadge', "select * from :_UserBadge");
+        } elseif ($ex->exists('YagaBadge')) {
+            // https://github.com/bleistivt/yaga
+            $map = [
+                'Description' => 'Body',
+                'RuleClass' => 'Type',
+                'RuleCriteria' => 'Attributes', // This probably doesn't actually work, but we'll try.
+                'AwardValue' => 'Points',
+                'Enabled' => 'Active',
+            ];
+            $ex->export('Badge', "select * from :_YagaBadge", $map);
+            $ex->export('UserBadge', "select * from :_YagaBadgeAward");
+        }
+    }
+
+    /**
+     * Ranks support for cloud + Yaga.
+     *
+     * @param ExportModel $ex
+     */
+    public function ranks(ExportModel $ex)
+    {
+        if ($ex->exists('Rank')) {
+            // Vanilla Cloud
+            $ex->export('Rank', "select * from :_Rank");
+        } elseif ($ex->exists('YagaRank')) {
+            // https://github.com/bleistivt/yaga
+            $map = [
+                'Description' => 'Body',
+                'Sort' => 'Level',
+                // Use 'Name' as both 'Name' and 'Label' (via SQL below)
+            ];
+            $ex->export('Rank', "select *, Name as Label from :_YagaRank", $map);
+        }
+    }
+
+    /**
+     * Reactions support for cloud + Yaga.
+     *
+     * @param ExportModel $ex
+     */
+    public function reactions(ExportModel $ex)
+    {
+        if ($ex->exists('ReactionType')) {
+            // Vanilla Cloud & later open source
+            $ex->export('ReactionType', "select * from :_ReactionType");
+            //$ex->export('Reaction', "select * from :_Tag where Type='Reaction'");
+            $ex->export('UserTag', "select * from :_UserTag");
+        } elseif ($ex->exists('YagaReaction')) {
+            // https://github.com/bleistivt/yaga
+            // Shortcut use of Tag table by setting ActionID = TagID.
+            // This wouldn't work for exporting a Yaga-based Vanilla install to a "standard" reactions Vanilla install,
+            // but I have to assume no one is using Porter for that anyway.
+            // Other Targets should probably directly join ReactionType & UserTag on TagID anyway.
+            $ex->export('ReactionType', "select *, ActionID as TagID from :_YagaAction"); // Name & Description only
+            $map = [
+                'ParentID' => 'RecordID',
+                'ParentType' => 'RecordType',
+                'InsertUserID' => 'UserID',
+                'ParentScore' => 'Total',
+                'ActionID' => 'TagID',
+            ];
+            $ex->export('UserTag', "select * from :_YagaReaction", $map);
+        }
+    }
+
+    /**
+     * Polls support for cloud + "DiscussionPolls".
+     *
+     * @param ExportModel $ex
+     */
+    public function polls(ExportModel $ex)
+    {
+        if ($ex->exists('Poll')) {
+            // SaaS
+            $ex->export('Poll', "select * from :_Poll");
+            $ex->export('PollOption', "select * from :_PollOption");
+            $ex->export('PollVote', "select * from :_PollVote");
+        } elseif ($ex->exists('DiscussionPolls')) {
+            // @todo https://github.com/hgtonight/Plugin-DiscussionPolls
+            //$ex->export('Poll', "select * from :_DiscussionPollQuestions");
+            //$ex->export('PollOption', "select * from :_DiscussionPollQuestionOptions");
+            //$ex->export('PollVote', "select * from :_DiscussionPollAnswers");
         }
     }
 }
