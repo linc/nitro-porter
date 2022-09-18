ebe7d64a505dcbf4f4549d078901c7205210e897
Lincoln Russell <lincoln@icrontic.com>
2022-09-18 14:41:35 -0400

    Flarum: Parse Giphy as an image embed

--- src/Parser/FlarumImageEmbed.php
+++ src/Parser/FlarumImageEmbed.php
@@ -18,7 +18,7 @@ class FlarumImageEmbed extends EmbedExternalListener
     public function process(Line $line)
     {
         $embed = $line->insertJsonKey('embed-external');
-        if ($embed && $this->isEmbedExternal($embed, 'image')) {
+        if ($embed && ($this->isEmbedExternal($embed, 'image') || $this->isEmbedExternal($embed, 'giphy'))) {
             $this->pick($line, ['url' => $embed['data']['url']]);
             $line->setDone();
         }
