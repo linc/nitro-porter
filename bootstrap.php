56f0e8539d91fc83ae2ef75d8b74866d65145ef1
Lincoln Russell <lincoln@icrontic.com>
2023-12-25 16:02:22 -0500

    Move CLI execution to bin file to prevent conflict with tools

--- bootstrap.php
+++ bootstrap.php
@@ -16,14 +16,3 @@ if (ini_get('date.timezone') == '') {
 // Load source & target support.
 \Porter\Support::getInstance()->setSources(loadSources());
 \Porter\Support::getInstance()->setTargets(loadTargets());
-
-// CLI app setup.
-$app = new Ahc\Cli\Application('Nitro Porter', 'v4.0');
-
-// Add commands.
-$app->add(new Porter\Command\ListCommand());
-$app->add(new Porter\Command\ShowCommand());
-$app->add(new Porter\Command\RunCommand());
-
-// Execute command.
-$app->handle($_SERVER['argv']);
