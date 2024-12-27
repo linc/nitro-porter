db400c4bddc9ab91994fad02967554c3c9cd4f1b
Lincoln Russell <lincoln@icrontic.com>
2024-12-27 14:41:19 -0500

    fix: Vanilla avatar filename processing

---
+++ tests/FilterTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace PorterTest;
+
+use PHPUnit\Framework\TestCase;
+
+final class FilterTest extends TestCase
+{
+    /**
+     * @covers \vanillaPhoto
+     */
+    public function testParseRender()
+    {
+        $photoTests = [
+            // Add 'p'
+            'userpics/396/YGIC427MJADQ.jpg' => 'userpics/396/pYGIC427MJADQ.jpg',
+            'uploads/userpics/820/4J95NK90AFDT.jpeg' => 'uploads/userpics/820/p4J95NK90AFDT.jpeg',
+            // No change; URL
+            'https://example.com/forum/uploads/userpics/396/YGIC427MJADQ.jpg'
+                => 'https://example.com/forum/uploads/userpics/396/YGIC427MJADQ.jpg',
+            // No change; not Vanilla origin - filename pattern
+            'userpics/avatar11_4.gif' => 'userpics/avatar11_4.gif',
+            // No change; not Vanilla origin - extension
+            'userpics/396/YGIC427MJADQ.ext' => 'userpics/396/YGIC427MJADQ.ext',
+        ];
+        foreach ($photoTests as $input => $expectedOutput) {
+            $testOutput = \vanillaPhoto($input);
+            $this->assertEquals($testOutput, $expectedOutput);
+        }
+    }
+}
