linc/nitro-porter

commit db5d542f99a2b63d921b372ea7e8aad45574fa42

fix: Force Flarum tag names to not be null.
See bug report: https://github.com/linc/nitro-porter/discussions/45#discussion-6264574
"FLA_tags name cannot be null"

src/Target/Flarum.php
@@ -332,6 +332,7 @@ class Flarum extends Target
         $query = $ex->dbImport()->table('PORT_Category')
             ->select(
                 '*',
+                $ex->dbImport()->raw('COALESCE(Name, CONCAT("category", id)) as name'), // Cannot be null.
                 $ex->dbImport()->raw("if(ParentCategoryID = -1, null, ParentCategoryID) as ParentCategoryID"),
                 $ex->dbImport()->raw("0 as is_hidden"),
                 $ex->dbImport()->raw("0 as is_restricted")

commit a6b36cc9fa2c913571b28d92481c1ad8b99c2e96

Fix column name error introduced in db5d542f99a2b63d921b372ea7e8aad45574fa42

src/Target/Flarum.php
@@ -334,7 +334,7 @@ class Flarum extends Target
         $query = $ex->dbImport()->table('PORT_Category')
             ->select(
                 '*',
-                $ex->dbImport()->raw('COALESCE(Name, CONCAT("category", id)) as name'), // Cannot be null.
+                $ex->dbImport()->raw('COALESCE(Name, CONCAT("category", CategoryID)) as name'), // Cannot be null.
                 $ex->dbImport()->raw("if(ParentCategoryID = -1, null, ParentCategoryID) as ParentCategoryID"),
                 $ex->dbImport()->raw("0 as is_hidden"),
                 $ex->dbImport()->raw("0 as is_restricted")