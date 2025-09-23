e87c7f458cbe0bd6fb5fa4e2b43aef2f45355928
Lincoln Russell <lincoln@icrontic.com>
2025-09-23 17:25:25 -0400

    dev: Rethink default config values for files

--- config-sample.php
+++ config-sample.php
@@ -9,11 +9,20 @@ return [
     'source_prefix' => '',
     'target_prefix' => '',
 
-    // Paths to folders (optional).
-    'source_attachments' => '',
-    'target_attachments' => '',
-    'source_avatars' => '',
-    'target_avatars' => '',
+    // Paths to local folders (optional, for if files need renaming).
+    // If the platform uses subfolders for thumbnails, the package should figure that out.
+    'source_attachments' => '~/source/files',
+    'target_attachments' => '~/target/files',
+    'source_avatars' => '~/source/avatars',
+    'target_avatars' => '~/target/avatars',
+
+    // Relative web path prefixed to attachments / files in the database (for links).
+    // Ex: if your imported attachments will be at https://example.com/uploads/imported/{filename},
+    //  then your target_webroot is 'uploads/imported/'.
+    // With these default settings, you would copy the entire folders `~/target/avatars`
+    //  and `~/target/attachments` into your forum's `uploads` folder after migrating.
+    'option_attachments_webroot' => '', // Flarum: 'uploads/files/',
+    'option_avatars_webroot' => '', // Flarum: 'uploads/avatars/',
 
     // Aliases of connections.
     // (If you're just editing the 2 default connections below, don't change these.)
